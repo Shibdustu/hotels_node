75bdd77f0e38de65f2dbe49d26512b7b2cf8c691
Shibdustu <shibdustu@gmail.com>
2024-09-16 12:22:05 +0530

    shift local mongoDB database to cloud mongoDB Atlas database

--- db.js
+++ db.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 //define the MongoDB connection URL
-const mongoURL = 'mongodb://localhost:27017/Hotels'
+//const mongoURL = process.env.MONGODB_URL_LOCAL // local data base
+const mongoURL = process.env.MONGODB_URL;
 // set up MongoDB connection
 mongoose.connect(mongoURL);
 // get the default mongoose.connection
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const db = require('./db');
+require('dotenv').config();
 
 const bodyParser= require('body-parser');
 app.use(bodyParser.json());
@@ -18,7 +19,7 @@ const menuItemRouter = require('./routes/menuItemRoutes');
 app.use('/person',personRoutes);
 app.use('/menu',menuItemRouter);
 
-
-app.listen(3000,function(){
+const PORT = process.env.PORT || 3000;
+app.listen(PORT,function(){
     console.log("Server in active on port 3000 ");
 });
